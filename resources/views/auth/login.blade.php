0d3919f0b1fb65d688f458225db80ef5d79b7aa4
Alexander Rhett Crammer <alexander2475914@gmail.com>
2015-11-26 14:26:26 -0500

    Missed a spot! Re-apply login form styles.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,16 +1,17 @@
 @extends('auth.master')
 @section('title', 'Login')
 @section('content')
-@if ($errors)
-<div class="probs">
-  @foreach ($errors->all() as $error)
-    <p class="prob">{{ $error }}</p>
-  @endforeach
-</div> <!-- .login-probs -->
-@endif
-  <form method="POST" action="/auth/login">
+  <form method="POST" action="/auth/login" class="login-form">
+    @if ($errors)
+    <div class="probs">
+      @foreach ($errors->all() as $error)
+        <p class="prob">{{ $error }}</p>
+      @endforeach
+    </div> <!-- .login-probs -->
+    @endif
     {!! csrf_field() !!}
     <input type="text" name="username" id="username" placeholder="Username" value="{{ old('username') }}">
+    <br />
     <input type="password" name="password" placeholder="Password" id="password">
     <input type="submit" class="button" value="Login">
   </form>
