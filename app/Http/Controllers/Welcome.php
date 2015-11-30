51c88233083e8688c4a769760fdfa5943839f148
Alexander Rhett Crammer <alexander2475914@gmail.com>
2015-11-30 14:31:01 -0500

    Add 'Welcome.php' Again
    This file disappeared.

---
+++ app/Http/Controllers/Welcome.php
@@ -0,0 +1,20 @@
+<?php
+namespace Eximius\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Eximius\Http\Requests;
+use Eximius\Http\Controllers\Controller;
+
+class Welcome extends Controller
+{
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    return view('welcome.index');
+  }
+}
